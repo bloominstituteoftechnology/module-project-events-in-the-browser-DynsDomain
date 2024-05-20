ecc7b7dc61e83b1490398cd59c2fe50afe621733
Trevon Nelson <Dyntrelle@gmail.com>
2024-05-20 06:38:04 -0500

    Game is successfully able to close

--- frontend/index.js
+++ frontend/index.js
@@ -73,6 +73,7 @@ function moduleProject2() {
       let isDown = evt.key === keys.down
       let isLeft = evt.key === keys.left
       let isRight = evt.key === keys.right
+      let isSpacebar = evt.key === keys.space
 
       if (isUp) {
         console.log('you clicked Up')
@@ -95,9 +96,31 @@ function moduleProject2() {
       }
           
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
-
+    else if (isSpacebar) {
+      let mosquito = targeted.firstChild
+        
+      if (mosquito && mosquito.dataset.status === 'alive') {
+        mosquito.dataset.status = 'dead'
+        mosquito.parentElement.style.backgroundColor = 'red'
+      }
+    }
     // 👉 TASK 5 - End the game 👈
     console.log('you clicked down a key')
+
+    let liveMosquitos = document.querySelectorAll('[data-status-alive]')
+    if(!liveMosquitos.length) {
+      let elapsed = getTimeElapsed()
+      document.querySelector('p.info').textContent =
+      `Extermination completed in ${elapsed / 1000} seconds!`
+
+      let restartBtn = document.createElement('button')
+      restartBtn.textContent = 'Restart'
+      restartBtn.addEvenetListener('click', () => {
+        location.reload()
+
+      })
+      document.querySelector('h2').insertAdjacentElement('beforeend',restartBtn)
+    }
   })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
